3179cfd4e7b5d9b2372d6a9e27ade01bd61b8dc0
carlos-mattos <ceamattos.jf@gmail.com>
2022-05-25 21:25:17 -0300

    scripts for tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   roots: ["<rootDir>/src"],
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!**/test/**",
+    "!**/config/**",
+  ],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "node",
